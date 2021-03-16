5a412fab5e81e98004e7f9b8a48142e0a9099006
Tobias Kuhn <kuhntobias@gmail.com>
2021-03-16 15:46:00 +0100

    Improve script

--- check.sh
+++ check.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 #
 # Usage:
-# $ ./check.sh
+# $ ./check.sh nanopubs/np1/cage_clusters_version2.trusty.trig.gz
 
-echo nanopubs/np1/trusty.cage_clusters_version2.mod.nq.gz
-np check nanopubs/np1/trusty.cage_clusters_version2.mod.nq.gz
-echo nanopubs/np2/trusty.gene_associations_version2.mod.nq.gz
-np check nanopubs/np2/trusty.gene_associations_version2.mod.nq.gz
-echo nanopubs/np3/trusty.ff_expressions_version2.mod.nq.gz
-np check nanopubs/np3/trusty.ff_expressions_version2.mod.nq.gz
+echo $1
+np check $1
 
