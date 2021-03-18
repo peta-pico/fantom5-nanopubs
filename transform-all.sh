c004ca93b37f24b6c5b6b48c7ecc7e3386071547
Tobias Kuhn <kuhntobias@gmail.com>
2021-03-18 09:17:42 +0100

    Improve script

--- transform-all.sh
+++ transform-all.sh
@@ -6,10 +6,15 @@
 # $ ./transform-all.sh nanopubs/np3/ff_expressions_version2 1044
 
 (for N in $(seq 1 1 $2); do gunzip -c $1_$N.nq.gz; done) \
-  | sed -r 's_^([^ ]+ <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.nanopub.org/nschema#Nanopublication> [^ ]+)> .$_\1#Head> ._' \
-  | sed -r 's_^([^ ]+ <http://www.nanopub.org/nschema#has(Assertion|Provenance|PublicationInfo)> [^ ]+ [^ ]+)> .$_\1#Head> ._' \
+  | sed -r 's!^([^ ]+ <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.nanopub.org/nschema#Nanopublication> [^ ]+)> .$!\1#Head> .!' \
+  | sed -r 's!^([^ ]+ <http://www.nanopub.org/nschema#has(Assertion|Provenance|PublicationInfo)> [^ ]+ [^ ]+)> .$!\1#Head> .!' \
+  | sed -r 's!XMLSchema#double> !XMLSchema#string> !g' \
+  | sed -r 's!<http://rdf.biosemantics.org/nanopubs/riken/fantom5/version_2/([^ #]+)#?([^ #]*)>!<http://purl.org/nanopub/temp/\1\2>!g' \
   | gzip \
   > $1.mod.nq.gz
 
-np mktrusty -o $1.trusty.trig.gz $1.mod.nq.gz
+np op filter -o $1.mod.trig $1.mod.nq.gz
+cat prefixes.trig $1.mod.trig | gzip > $1.mod.trig.gz
+np mktrusty -o $1.trusty.trig.gz $1.mod.trig.gz
 
+#rm $1.mod.nq.gz $1.mod.trig $1.mod.trig.gz
